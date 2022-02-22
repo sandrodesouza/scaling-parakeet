9fa4db1cc4d9663ade6510ed25fd9407553d2695
Sandro <sandro.desouza@cloudhesive.com>
2022-02-22 00:50:55 -0300

    clean up TODOs

--- services/app1/src/http/loans/get/schema.ts
+++ services/app1/src/http/loans/get/schema.ts
@@ -1,4 +1,108 @@
 /**
+ * @schema CompanyModel
+ * required:
+ *   - id
+ * properties:
+ *   id:
+ *     type: string
+ *   BTW:
+ *     type: string
+ *   LEI:
+ *     type: string
+ *   RSIN:
+ *     type: string
+ *   actief:
+ *     type: boolean
+ *   bestaandehandelsnaam:
+ *     type: array
+ *     items:
+ *       type: string
+ *   dossiernummer:
+ *     type: string
+ *   handelsnaam:
+ *     type: string
+ *   huisnummer:
+ *     type: string
+ *   locatie:
+ *     type: object
+ *     properties:
+ *       lat:
+ *         type: string
+ *       lon:
+ *         type: string
+ *   pand_id:
+ *     type: string
+ *   plaats:
+ *     type: string
+ *   postcode:
+ *     type: string
+ *   sbi:
+ *     type: array
+ *     items:
+ *       type: string
+ *   statutairehandelsnaam:
+ *     type: array
+ *     items:
+ *       type: string
+ *   straat:
+ *     type: string
+ *   subdossiernummer:
+ *     type: string
+ *   type:
+ *     type: string
+ *   vbo_id:
+ *     type: string
+ *   vestigingsnummer:
+ *     type: string
+ */
+const CompanyModel = {
+  type: 'object',
+  properties: {
+    id: { type: 'string' },
+    BTW: { type: 'string' },
+    LEI: { type: 'string' },
+    RSIN: { type: 'string' },
+    actief: { type: 'boolean' },
+    bestaandehandelsnaam: {
+      type: 'array',
+      items: {
+        type: 'string',
+      },
+    },
+    dossiernummer: { type: 'string' },
+    handelsnaam: { type: 'string' },
+    huisnummer: { type: 'string' },
+    locatie: {
+      type: 'object',
+      properties: {
+        lat: { type: 'string' },
+        lon: { type: 'string' },
+      },
+    },
+    pand_id: { type: 'string' },
+    plaats: { type: 'string' },
+    postcode: { type: 'string' },
+    sbi: {
+      type: 'array',
+      items: {
+        type: 'string',
+      },
+    },
+    statutairehandelsnaam: {
+      type: 'array',
+      items: {
+        type: 'string',
+      },
+    },
+    straat: { type: 'string' },
+    subdossiernummer: { type: 'string' },
+    type: { type: 'string' },
+    vbo_id: { type: 'string' },
+    vestigingsnummer: { type: 'string' },
+  },
+}
+
+/**
  * @schema ListLoanResponse
  * required:
  *   - id
@@ -12,10 +116,8 @@
  *   createdAt:
  *     type: string
  *   company:
- *     type: object
+ *     $ref: "#/components/schemas/CompanyModel"
  */
-
-// TODO: more details on company schema
 
 const ListLoanResponse = {
   type: 'object',
@@ -24,37 +126,7 @@ const ListLoanResponse = {
     amount: { type: 'number' },
     status: { type: 'string' },
     createdAt: { type: 'string' },
-    company: {
-      type: 'object',
-      properties: {
-        id: { type: 'string' },
-        BTW: { type: 'string' },
-        LEI: { type: 'string' },
-        RSIN: { type: 'string' },
-        actief: { type: 'boolean' },
-        // bestaandehandelsnaam: Joi.array(), // TODO: arrays
-        dossiernummer: { type: 'string' },
-        handelsnaam: { type: 'string' },
-        huisnummer: { type: 'string' },
-        locatie: {
-          type: 'object',
-          properties: {
-            lat: { type: 'string' },
-            lon: { type: 'string' },
-          },
-        },
-        pand_id: { type: 'string' },
-        plaats: { type: 'string' },
-        postcode: { type: 'string' },
-        // sbi: Joi.array(),
-        // statutairehandelsnaam: Joi.array(),
-        straat: { type: 'string' },
-        subdossiernummer: { type: 'string' },
-        type: { type: 'string' },
-        vbo_id: { type: 'string' },
-        vestigingsnummer: { type: 'string' },
-      },
-    },
+    company: CompanyModel,
   },
   required: ['id'],
   additionalProperties: false,
